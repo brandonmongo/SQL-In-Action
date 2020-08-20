ca68857f8fd3bef88521dfbb5297ece089dd33dc
brandonmongo <brandonmongo@hotmail.com>
2020-08-20 14:03:42 +0000

    Connecting To MySQL From Python

---
+++ mysql-from-python.py
@@ -0,0 +1,24 @@
+import os
+import pymysql
+
+#Get username from Gitpod workspace 
+
+username = os.getenv("C9_USER")
+
+# Connect to the database
+# connection object to connect to the database
+connection = pymysql.connect(host="localhost",
+                             user=username,
+                             password="",
+                             db="Chinook")
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        sql = "select * from Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection, regardless of whether the above was succussful
+    connection.close()
