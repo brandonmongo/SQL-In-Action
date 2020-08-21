ea2bb358f11129c68f36a33731522f2a3ccd6f8c
brandonmongo <brandonmongo@hotmail.com>
2020-08-21 18:03:03 +0000

    Insert

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -15,10 +15,9 @@ connection = pymysql.connect(host="localhost",
 try:
     # Run a query
     with connection.cursor() as cursor:
-        cursor.execute(""" CREATE TABLE IF NOT EXISTS
-                        Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the
-        # table already exist
+        row = ("Bob", 21, "1999-02-06 23:04:56")
+        cursor.execute("insert into Friends values (%s, %s, %s);", row)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was succussful
     connection.close()
